c2c19b591218ecf34f8190a07fd69b2ecdde90f1
aviram <aviram2n@gmail.com>
2025-02-03 03:37:08 +0200

    test node linter with prettier

--- server/nodeJs_server/main.mjs
+++ server/nodeJs_server/main.mjs
@@ -1,50 +1,27 @@
-import express from 'express';
-import { readFile } from 'fs/promises';
-import cors from 'cors';
-import { quickSort } from './quickSort.mjs';
-import { arrOf1kNum , arrOf10kNum , arrOf100kNum , mil , arrOfHalfMil} from './arrays.mjs';
+import express from "express";
+import { readFile } from "fs/promises";
+import cors from "cors";
+import { quickSort } from "./quickSort.mjs";
+import { arrOfHalfMil } from "./arrays.mjs";
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/quickSort/sorted/500', (req, res) => { 
-  const sortedArr = (quickSort(arrOfHalfMil)) 
-  console.log (sortedArr)
-  res.send((quickSort(arrOfHalfMil)));
+app.get("/quickSort/sorted/500", (req, res) => {
+  const sortedArr = quickSort(arrOfHalfMil);
+  console.log(sortedArr);
+  res.send(quickSort(arrOfHalfMil));
 });
 
-app.get('/JSjson/10mb', async (req, res) => {
+app.get("/JSjson/10mb", async (req, res) => {
   try {
-    const JSdata = await readFile('../jsons/10mb.json', 'utf8');
+    const JSdata = await readFile("../jsons/10mb.json", "utf8");
     res.json(JSON.parse(JSdata));
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
